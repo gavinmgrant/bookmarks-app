3bda24c412e936392500f9f39d5e6dded16807c3
Gavin Grant <gavingrant@gmail.com>
2020-07-14 16:24:24 -0700

    installed react-router-dom

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_KEY: '$2a$10$5n2.4ieWS6SoYB6ebY4ri.8AK4kgWrXLcSgEXC65.U3yZaMme7Iz2',
 }
